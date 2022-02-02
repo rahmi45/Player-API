46730c985abb92814f77a196fd8ca99f96c8bf97
RAHMI <rachid.rahmi@atos.net>
2022-02-02 22:46:39 +0100

    update project

--- src/main/java/com/spring/playerapi/controller/PlayerController.java
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -1,10 +1,16 @@
 package com.spring.playerapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api")
 public class PlayerController {
-
+	
+	@GetMapping(value = "/data")
+	//http://localhost:8088/playerapi/api/data
+	public String getData() {
+		return "myData";
+	}
 }
