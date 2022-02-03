4e0181e6dfac4b965e26c046b347d2d317e7c12d
RAHMI <rachid.rahmi@atos.net>
2022-02-03 21:37:33 +0100

    get player by Id from DB

--- src/main/java/com/spring/playerapi/controller/PlayerController.java
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,9 +26,18 @@ public class PlayerController {
 	
 	// add mapping for GET/ players
     @GetMapping("/players")
-    public List<Player> getPlayerss(){
+    public List<Player> getPlayers(){
 
         return palyerService.allPlayer();
 
+    }
+    
+ // add mapping for GET/ customers
+    @GetMapping("/players/{playerId}")
+    public Player getPlayerId(@PathVariable int playerId){
+
+    	Player player =  palyerService.showPlayer(playerId);
+
+        return player;
     }
 }
