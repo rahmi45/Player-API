6d00737c039291a7889f5d462c8340a925948a55
RAHMI <rachid.rahmi@atos.net>
2022-02-04 21:44:13 +0100

    Add Player

--- src/main/java/com/spring/playerapi/controller/PlayerController.java
+++ src/main/java/com/spring/playerapi/controller/PlayerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +19,7 @@ import com.spring.playerapi.service.PlayerService;
 public class PlayerController {
 	
 	@Autowired
-	private PlayerService palyerService;
+	private PlayerService playerService;
 	
 	@GetMapping(value = "/data")
 	//http://localhost:8088/playerapi/api/data
@@ -29,18 +31,29 @@ public class PlayerController {
     @GetMapping("/players")
     public List<Player> getPlayers(){
 
-        return palyerService.allPlayer();
+        return playerService.allPlayer();
 
     }
     
- // add mapping for GET/ customers
+ // add mapping for GET/ players
     @GetMapping("/players/{playerId}")
     public Player getPlayerId(@PathVariable int playerId){
 
-    	Player player =  palyerService.showPlayer(playerId);
+    	Player player =  playerService.showPlayer(playerId);
     	if(player == null){
             throw new PlayerNotFoundException("Player with the following id does not exist in the database. id : " + playerId);
     	}
+        return player;
+    }
+    
+ // add mapping for POST/ players - add a new player
+    @PostMapping("/players")
+    public Player addPlayer(@RequestBody Player player){
+
+        // set the id of the player id to 0
+        // this will force to make an insert instead of update
+    	player.setId(0);
+    	playerService.savePlayer(player);
         return player;
     }
 }
